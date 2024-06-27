2c97a273813d185dad461a664ab2e4d7cf0059c7
trangmau <111789871+trangmau@users.noreply.github.com>
2024-06-27 21:13:23 +0700

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,4 +1,3 @@
-# Uncomment the imports below before you add the function code
 import requests
 import os
 from dotenv import load_dotenv
@@ -11,12 +10,14 @@ sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
     default="http://localhost:5050/")
 
-# def get_request(endpoint, **kwargs):
+'# def get_request(endpoint, **kwargs):'
 # Add code for get requests to back end
+
+
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
+        for key, value in kwargs.items():
             params=params+key+"="+value+"&"
 
     request_url = backend_url+endpoint+"?"+params
@@ -26,12 +27,14 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
         # If any error occurs
         print("Network exception occurred")
-# def analyze_review_sentiments(text):
-# request_url = sentiment_analyzer_url+"analyze/"+text
+'# def analyze_review_sentiments(text):'
+'# request_url = sentiment_analyzer_url+"analyze/"+text'
 # Add code for retrieving sentiments
+
+
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
@@ -41,13 +44,15 @@ def analyze_review_sentiments(text):
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
-# def post_review(data_dict):
+'# def post_review(data_dict):'
 # Add code for posting review
+
+
 def post_review(data_dict):
     request_url = backend_url+"/insert_review"
     try:
         response = requests.post(request_url,json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception as e:
         print("Network exception occurred")
