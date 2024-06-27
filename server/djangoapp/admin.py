a924a3cf41c6fa0ae9ed533da2efa47d25e7474d
trangmau <111789871+trangmau@users.noreply.github.com>
2024-06-27 20:25:45 +0700

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,12 +5,14 @@ from .models import CarMake, CarModel
 
 # CarModelInline class
 
+
 class CarModelInline(admin.TabularInline):
     model = CarModel
     # Number of empty forms to display for adding new CarModel instances
     extra = 1
 
 # CarModelAdmin class
+
 
 @admin.register(CarModel)
 class CarModelAdmin(admin.ModelAdmin):
@@ -36,6 +38,7 @@ class CarModelAdmin(admin.ModelAdmin):
         return super().get_queryset(request).select_related('car_make')
 
 # CarMakeAdmin class with CarModelInline
+
 
 @admin.register(CarMake)
 class CarMakeAdmin(admin.ModelAdmin):
