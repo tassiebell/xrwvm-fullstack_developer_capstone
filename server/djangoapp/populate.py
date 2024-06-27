5a73291f033bcdf6d530302914ad9c2e8abb485a
trangmau <111789871+trangmau@users.noreply.github.com>
2024-06-27 21:04:41 +0700

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -14,8 +14,10 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-        car_make_instances.append(CarMake.objects.create(name=data['name'],
-             description=data['description']))
+        car_make_instances.append(
+            CarMake.objects.create(
+                name=data['name'],description=data['description'])
+        )
 
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
@@ -49,11 +51,10 @@ def initiate():
        "car_make": car_make_instances[4]},
       {"name": "Kluger", "type": "SUV", "year": 2023,
        "car_make": car_make_instances[4]},
-    # Add more CarModel instances as needed
+      # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
         CarModel.objects.create(name=data['name'],
                                 car_make=data['car_make'],
-                                type=data['type'], year=data['year'])
-        
+                                type=data['type'], year=data['year'])     
