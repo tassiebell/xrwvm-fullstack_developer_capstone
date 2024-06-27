3ae5b3ed53b31204bb313d39ad9951dfdda8b95f
trangmau <111789871+trangmau@users.noreply.github.com>
2024-06-27 21:26:10 +0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-#from .restapis import get_request, analyze_review_sentiments, post_review
+'#from .restapis import get_request, analyze_review_sentiments, post_review'
 
 app_name = 'djangoapp'
 urlpatterns = [
@@ -12,18 +12,17 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='register'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     # path for dealer reviews view
     path(route='reviews/dealer/<int:dealer_id>',
          view=views.get_dealer_reviews, name='dealer_details'),
-    #path for get dealerships
+    '#path for get dealerships'
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>',
-         view=views.get_dealerships,name='get_dealers_by_state'),
+         view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>',
          view=views.get_dealer_details, name='dealer_details'),
     # path for add a review view
-    path(route='add_review', view=views.add_review, name='add_review'),
-    
+    path(route='add_review', view=views.add_review, name='add_review'),   
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
