07609eddeeeb76047aa2e0adbc193d6aaec16123
Thy Tran <thytran222@gmail.com>
2024-06-28 17:34:41 -0400

    Adding temporary changes to Github

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -161,7 +161,7 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous is False):
+    if (request.user.is_anonymous == False):
         data = json.loads(request.body)
         try:
             response = post_review(data)
