b56f87ee2a40ce15a682650ae2549eeb34eecec6
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-27 02:56:55 +0200

    Update App.js

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,18 +1,11 @@
-# Uncomment the imports before you add the code
-# from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-# from . import views
+import LoginPanel from "./components/Login/Login"
+import { Routes, Route } from "react-router-dom";
 
-app_name = 'djangoapp'
-urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPanel />} />
+    </Routes>
+  );
+}
+export default App;
