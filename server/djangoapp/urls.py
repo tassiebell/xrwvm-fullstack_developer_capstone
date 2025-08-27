55222cb6eb901b8f422ac1fd0c74c52f8fd24eb4
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-27 02:58:45 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,26 +1,18 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
+# Uncomment the imports before you add the code
+# from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+# from . import views
 
+app_name = 'djangoapp'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # # path for registration
+
+    # path for login
+    # path(route='login', view=views.login_user, name='login'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
