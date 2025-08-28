f4218282b7d6f43a2b4c80e3bb162fe355d8cc80
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-28 03:03:01 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,10 +1,10 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib import admin
-from .models import CarMake, CarModel
+
 
 
 # Create your models here.
@@ -44,6 +44,4 @@ class CarModel(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-        # Registering models with their respective admins
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
