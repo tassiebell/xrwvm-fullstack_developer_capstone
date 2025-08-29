ca55a1b341f9eb018b9e70dbccaf0e3c3ce7bc83
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-29 02:13:09 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -27,9 +27,19 @@ SECRET_KEY =\
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+ALLOWED_HOSTS = ['localhost', 'https://enddyogbenyi-8000.theiadockernext-' +
+                 '0-labs-' + 'prod-theiak8s-4-tor01.proxy.cognitive' +
+                 'class.ai', 'https://enddyogbenyi-8000.theianext-0-labs-' +
+                 'prod-misc-tools-us-east-0.proxy.cognitive' +
+                 'class.ai', 'https://enddyogbenyi-8000.theiadockernext-' +
+                 '1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
-ALLOWED_HOSTS=['localhost','https://novozhenova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=['https://novozhenova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://enddyogbenyi-8000.theiadockernext-0-labs-' +
+                        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
+                        'https://enddyogbenyi-8000.theianext-0-labs-prod-' +
+                        'misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                        'https://enddyogbenyi-8000.theiadockernext-1-labs-' +
+                        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,11 +71,10 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/static'),
-            os.path.join(BASE_DIR, 'frontend/build'),
-            os.path.join(BASE_DIR, 'frontend/build/static'),
-        ],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend/static'),
+                 os.path.join(BASE_DIR, 'frontend/build'),
+                 os.path.join(BASE_DIR, 'frontend/build/static'),],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -94,19 +103,23 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' +
+        'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'django.contrib.auth.password_validation.' +
+        'MinimumLengthValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'django.contrib.auth.password_validation.' +
+        'CommonPasswordValidator',
     },
     {
         'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'django.contrib.auth.password_validation.' +
+        'NumericPasswordValidator',
     },
 ]
 
