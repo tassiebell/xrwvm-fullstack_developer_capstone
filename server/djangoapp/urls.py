f2e583e25b9e5dcae009153bd1e3a8748c321399
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-29 17:14:54 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,8 +7,7 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration', view=views.registration, name='registration'),
-
+    path(route='register', view=views.registration, name='register'),
     # path for login
     path(route='login', view=views.login_user, name='login'),
 
@@ -16,7 +15,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
 
     # path for get cars
-    path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for get dealers
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
