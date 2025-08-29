48cae1a434949e41f8cc8fdf5c93cc3a02ac97e9
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-29 17:13:58 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,10 +29,8 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://orneliemanz-8000.theiadockernext-0-'
-                 'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://orneliemanz-8000.theiadockernext-0-'
-                        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://novozhenova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://novozhenova-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
