2510202214105189351fe55015ecfc4ba9cbc3b9
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-08-31 21:05:40 +0200

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,34 +22,14 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    
-    path('', 
-         TemplateView.as_view(template_name="Home.html")),
-    
-    path('about/',
-         TemplateView.as_view(template_name="About.html")),
-    
-    path('contact/', 
-         TemplateView.as_view(template_name="Contact.html")),
-    
-    path('login/', 
+    path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
-    
-    path('register/', 
-         TemplateView.as_view(template_name="index.html")),
-    
-    path('dealers/', 
-         TemplateView.as_view(template_name="index.html")),
-    
-    # fixing error (E501 line too long 
-    
-    # (xxx > 79 characters)) in github actions
-    
-    path('dealer/<int:dealer_id>', 
-         TemplateView.as_view(template_name="index.html")),
-    
     path('postreview/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
-    
-] + static(settings.STATIC_URL,
-           document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
