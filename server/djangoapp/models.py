e206ada72feb3ac9ef3ade9531a8104370018bea
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-09-01 15:13:32 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,6 +7,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     def __str__(self):
         return self.name
 
@@ -31,5 +32,6 @@ class CarModel(models.Model):
                                    MaxValueValidator(2023),
                                    MinValueValidator(2015)
                                 ])
+    
     def __str__(self):
         return self.name
