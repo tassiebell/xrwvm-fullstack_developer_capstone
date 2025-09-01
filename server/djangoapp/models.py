39696a3b3dc813d3592daec83d02aa96c3034909
Katerina Novozhenova <43950040+tassiebell@users.noreply.github.com>
2025-09-01 14:17:31 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,13 +1,12 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
@@ -16,19 +15,18 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
         ('HATCHBACK','Hatchback'),
-        ('COUPE','Coupe'),
-        ('MINIVAN','Minivan'),
-        ('CONVERTIBLE','Convertible'),
-        ('PICKUP','Pickup'),
+        ('COUPE', 'Coupe'),
+        ('MINIVAN', 'Minivan'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('PICKUP', 'Pickup'),
         # Add more choices as required
     ]
     type = models.CharField(max_length=11, choices=CAR_TYPES, default='SUV')
@@ -39,8 +37,8 @@ class CarModel(models.Model):
         ])
     # Other fields as needed
 
-    def __str__(self):
-        return self.name  # Return the name as the string representation
+def __str__(self):
+    return self.name  # Return the name as the string representation
 
         # Registering models with their respective admins
 
